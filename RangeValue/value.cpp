a9ef42290fa72c3af0c87c1c5b0e5f42c307451d
DeadShot0x7 <68690437+Deadshot0x7@users.noreply.github.com>
2022-06-15 10:49:31 +0530

    Added

--- RangeValue/value.cpp
+++ RangeValue/value.cpp
@@ -1,2 +1,18 @@
-#include<stdio.h>
-#include<conio.h>
+class Rnagrvalue
+{
+    public:
+    
+
+    void num(int x, int n)
+    {
+        switch(x)
+        {
+            try {
+   /   
+        } catch (const std::exception& e) {
+     std::cout << e.what(); // information from error printed
+}
+
+        }
+    }
+}
